6b2cf956657055941fcaaf24c9c01d6eaf144f16
jpablorl87 <glcsmp@gmail.com>
2025-04-01 11:57:57 -0500

    Invisible Power Mechanic ready

--- GJamLags/Assets/Scripts/Player/PlayerMovement.cs
+++ GJamLags/Assets/Scripts/Player/PlayerMovement.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerMovement : MonoBehaviour
 {
@@ -9,41 +10,58 @@ public class PlayerMovement : MonoBehaviour
     public Rigidbody2D rb;
     [SerializeField] private float speed = 5f;
     private Vector2 movement;
-    [Header("Invisible")]
+    [Header("Invisible Mechanic")]
     [SerializeField] private SpriteRenderer sprite;
     [SerializeField] private Color color1, color2;
     public static bool isVisible;
     public static bool canInvisible;
-    public static float powerDuration = 2f;
-    public static float powerCoolDown = 2f;
+    public static float powerDuration = 5f;
+    public static float powerCoolDown = 5f;
+    [Header("Invisible Power")]
+    [SerializeField] private Image invisibleBar;
+    [SerializeField] private float invisiblePoints;
     [Header("Dash")]
     [SerializeField] private float dashSpeed = 50f;
     [SerializeField] private bool canDash;
     [SerializeField] private bool isDashing;
     [SerializeField] private float dashDuration = 0.5f;
     [SerializeField] private float dashCoolDown = 0.5f;
-
-
+    [Header("Healt")]
+    [SerializeField] private float healthPoints;
+    [SerializeField] private Image healthView;
+    [SerializeField] private float damage = 1f;
+    private float timer;
     void Start()
     {
         canDash = true;
         isVisible = true;
         canInvisible = true;
+        invisiblePoints = 0f;
+        healthPoints = 4f;
         rb = GetComponent<Rigidbody2D>();
         sprite = GetComponent<SpriteRenderer>();
     }
 
     void Update()
     {
+        //If is dashing or pause, stop every mechanic
         if (isDashing) return;
+        //Inputs for movement
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
+        //Dash Coroutine
         if (Input.GetKeyDown(KeyCode.C) && canDash) StartCoroutine(Dash());
+        //Invisible Coroutine
         if (Input.GetKeyDown(KeyCode.Space) && canInvisible) StartCoroutine(Invisible());
+        timer = Mathf.Clamp(timer, 0, 5);
+        if (canInvisible) InvisiblePower();
+        else LoadPower();
     }
     private void FixedUpdate()
     {
+        //If is dashing or pause, stop every mechanic
         if (isDashing) return;
+        //Movement method
         MovePlayer();
     }
     //Movement Mechanic
@@ -60,6 +78,7 @@ public class PlayerMovement : MonoBehaviour
         Debug.Log(isVisible);
         yield return new WaitForSeconds(powerDuration);
         isVisible = true;
+        Debug.Log(isVisible);
         sprite.color = color1;
         yield return new WaitForSeconds(powerCoolDown);
         canInvisible = true;
@@ -75,5 +94,25 @@ public class PlayerMovement : MonoBehaviour
         isDashing = false;
         yield return new WaitForSeconds(dashCoolDown);
         canDash = true;
+    }
+    //Health
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Damage"))
+        {
+            healthPoints -= damage;
+            healthView.fillAmount = healthPoints / 4f;
+        }
+    }
+    //Invisible power
+    public void InvisiblePower()
+    {
+        invisiblePoints = 5f;
+        invisibleBar.fillAmount = invisiblePoints / 5;
+    }
+    public void LoadPower()
+    {
+        invisiblePoints = 0f;
+        invisibleBar.fillAmount = invisiblePoints / 5;
     }
 }
